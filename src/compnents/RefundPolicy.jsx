13a8259690abd9e1cfe7cd250c4f859364a31b59
AnshulDogra001 <abhinav.dogra001@gmail.com>
2025-03-21 00:41:38 +0530

    final

--- src/compnents/RefundPolicy.jsx
+++ src/compnents/RefundPolicy.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from "../assets/reachify (32).png";
+import logo from "../assets/Reachify (32).png";
 import { Link } from 'react-router-dom';
 
 export default function RefundPolicy() {
